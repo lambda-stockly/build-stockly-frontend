fb7d289cf5440bb4edaaa8ecfba220ac6f0908b5
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-05-20 11:07:32 -0700

    main appliaction dashboard after user login

---
+++ src/components/StocklyApp/StocklyApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './StocklyApp.scss';
+
+class StocklyApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Your Dashboard</h1>
+        <p>url should be /home</p>
+      </div>
+    );
+  }
+}
+export default StocklyApp;
