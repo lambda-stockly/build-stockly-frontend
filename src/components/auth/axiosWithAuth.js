3d4be1ed5c814b969699825f6faf8b7b58e48537
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-05-20 13:19:46 -0700

    axios with auth

--- src/components/auth/axiosWithAuth.js
+++ src/components/auth/axiosWithAuth.js
@@ -7,6 +7,6 @@ export const axiosWithAuth = () => {
       // 'Content-Type': 'application/json'
       Authorization: token
     },
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://stockly-backend.herokuapp.com'
   });
 };
