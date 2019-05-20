a7402a1868af4fdd70725471c2dda16bee020428
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-05-20 13:21:09 -0700

    axios with aust

--- src/components/auth/axiosWithAuth.js
+++ src/components/auth/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
   return axios.create({
     headers: {
-      // 'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
       Authorization: token
     },
     baseURL: 'https://stockly-backend.herokuapp.com'
